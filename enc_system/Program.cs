1a62d2d9dd5125e8294a39e21920831b5709061b
idadeveloping <ida.developing@gmail.com>
2019-06-06 22:48:07 +0300

    hello world test

--- enc_system/Program.cs
+++ enc_system/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace enc_system
+﻿using System;
+
+namespace enc_system
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello world test");
         }
     }
 }
